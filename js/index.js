65ef6e9066b954b5ccbb80b4e46f91f410c87f75
Muhammad Rafli Firmansyah <mrfirmansyah47@gmail.com>
2026-02-02 12:00:29 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'i have something'.split('')
+const text = 'i love you syifa'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
