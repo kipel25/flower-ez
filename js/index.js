540553638ff99b4260b31e3d42ea174bcc1ba4a6
Muhammad Rafli Firmansyah <mrfirmansyah47@gmail.com>
2026-02-02 12:40:31 +0700

    Update text string in index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'i have something for u'.split('')
+const text = 'i have something'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
